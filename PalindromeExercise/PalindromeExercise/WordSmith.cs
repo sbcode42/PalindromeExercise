61ad40e616e4a5bb5ff2ba99a290ac3060f88784
sbcode42 <ballersabooth@yahoo.com>
2020-10-16 16:11:49 -0500

    finally pushing from save

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            //reverse the word
+            
+            var reversed = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word[i];
+            }
+            //compare the original with the reversed 
+            if(reversed.ToLower() == word.ToLower())
+            { 
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            //return whether they are the same or not
+
+        }
+
+        
+            
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,18 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("Racecar", true)]
+        public void MyTest(string word, bool expected)
         {
-
+            //arange
+            var tester = new WordSmith();
+            //act
+            var actual = tester.IsAPalindrome(word);
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
